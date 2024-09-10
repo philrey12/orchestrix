e17fff8217dbbb7ca0ef87dbd236802fa23db6d0
philrey12 <devphil.work@gmail.com>
2024-09-10 22:31:19 +0800

    style: reset global styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Test
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Test
+    </main>
   );
 }
