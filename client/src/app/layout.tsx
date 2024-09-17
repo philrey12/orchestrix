f54a11dc98dbf2eeff01dca3e92dfd7201b777db
philrey12 <devphil.work@gmail.com>
2024-09-17 13:10:30 +0800

    build: add navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import DashboardWrapper from "./DashboardWrapper";
+import DashboardWrapper from "./dashboardWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
